f2054716561e547790bc237fd1488c825f4d400a
TheLokj <61231354+TheLokj@users.noreply.github.com>
2022-09-05 10:49:57 +0200

    Add rstudio.sh

---
+++ rstudio.sh
@@ -0,0 +1,12 @@
+sudo gpg --keyserver keyserver.ubuntu.com --recv-keys 3F32EE77E331692F
+
+sudo apt-get -y install r-base
+
+sudo apt-get -y install libnss3
+
+
+sudo wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-2022.07.1-554-amd64.deb
+
+sudo apt-get -y install ./rstudio-2022.07.1-554-amd64.deb
+
+sudo rm rstudio-2022.07.1-554-amd64.deb
